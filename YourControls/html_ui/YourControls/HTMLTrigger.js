fbee576a967431f01e0b88300e321f78468531ee
thordur03 <34524306+thordur03@users.noreply.github.com>
2021-10-16 20:09:01 +0000

    Add files via upload

---
+++ YourControls/html_ui/YourControls/HTMLTrigger.js
@@ -0,0 +1,36 @@
+const clickEvents = ["click", "mouseup", "mousedown"].map(eventType => {
+    let evt = document.createEvent("MouseEvents");
+    evt.YC = true
+    evt.initEvent(eventType, true, true);
+    return evt
+})
+const inputEvent = new Event('input', { bubbles: true});
+const nativeInputSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, "value").set
+
+class YourControlsHTMLTrigger {
+    static setInput(element, value) {
+        nativeInputSetter.call(element, value);
+        element.dispatchEvent(inputEvent);
+    }
+
+    static setPanel(eventName, instrumentName) {
+        const split = eventName.indexOf("#")
+        const targetInstrumentName = eventName.substring(0, split)
+
+        if (targetInstrumentName != instrumentName) {
+            return
+        }
+
+        const buttonName = eventName.substring(split + 1)
+        const button = document.getElementById(buttonName)
+
+        clickEvents.forEach(evt => {
+            button.dispatchEvent(evt)
+        });
+    }
+
+    static processInput(element, value) {
+        YourControlsHTMLTrigger.nativeInputSetter.call(element, value);
+        element.dispatchEvent(inputEvent);
+    }
+}
